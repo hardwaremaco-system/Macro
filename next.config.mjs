02b1cece90d6025487ab252c604d5e9553373037
hardwaremaco-system <hardwaremaco@gmail.com>
2026-06-10 21:47:22 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,13 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
-        pathname: '/**',
       },
     ],
   },
