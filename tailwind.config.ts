7b629100e74a2c123bcc71e4a7c80e32e7063af6
hardwaremaco-system <hardwaremaco@gmail.com>
2026-06-11 11:50:25 +0300

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+// tailwind.config.ts
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
