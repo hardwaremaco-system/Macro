4b9f2cf0d406a1e7eaf07b75550cae2debee7959
hardwaremaco-system <hardwaremaco@gmail.com>
2026-06-11 09:34:43 +0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,7 @@ import type { Config } from 'tailwindcss';
 const config: Config = {
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}", 
     "./src/context/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/lib/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/store/**/*.{js,ts,jsx,tsx,mdx}",
