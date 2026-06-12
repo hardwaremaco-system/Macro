f2dcd6f42ad834dc514d61292f10f301dda1de3d
hardwaremaco-system <hardwaremaco@gmail.com>
2026-06-12 06:52:06 +0300

    Update layout.tsx

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 // Forced relative paths
 import Header from '../../components/shop/Header';
 import Footer from '../../components/shop/Footer';
+import FloatingWhatsApp from '../../components/ui/FloatingWhatsApp';
 
 export default function ShopLayout({
   children,
@@ -10,12 +11,14 @@ export default function ShopLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen relative">
       <Header />
       <main className="flex-grow bg-gray-50">
         {children}
       </main>
       <Footer />
+      {/* Placed at the bottom so it floats over everything */}
+      <FloatingWhatsApp />
     </div>
   );
 }
